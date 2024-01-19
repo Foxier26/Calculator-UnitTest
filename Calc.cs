dc4c3044c73b4f4a992ed1bc7f32f41819d3c8e9
Андрей <122528091+Foxier26@users.noreply.github.com>
2024-01-19 12:14:24 +0300

    Файл калькулятора

---
+++ Calc.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassLibrary1
+{
+    public class Calc
+    {
+        public double Sum(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Minus(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                throw new DivideByZeroException();
+            }
+                
+            return x / y;
+        }
+    }
+}
